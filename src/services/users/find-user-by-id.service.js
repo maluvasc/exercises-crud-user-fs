b4cd5e0105d54d6f0ceb32c13adfaa3cec531733
Felipe Rosas <contato@eufelipe.com>
2022-06-14 12:31:45 -0300

    chore: add find users by id service

---
+++ src/services/users/find-user-by-id.service.js
@@ -0,0 +1,8 @@
+async function findUserById(id) {
+  /*
+  - TODO 25: Deve retornar NULL se o usuário com o email passado nao existir;
+  - TODO 26: Deve retornar um usuário válido se ele existir nno banco; 
+*/
+}
+
+module.exports = { findUserById };
