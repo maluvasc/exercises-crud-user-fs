48c7ea5ec11137e7dd2205b5e2c014ed6440d61b
Felipe Rosas <contato@eufelipe.com>
2022-06-14 12:29:53 -0300

    chore: add update user service

---
+++ src/services/users/update-user.service.js
@@ -0,0 +1,9 @@
+async function updateUser(id, { name, email, phone }) {
+  /*
+  - TODO 29: Deve retornar uma exceção de erro "User ID is required" caso não tenha passado ID;
+  - TODO 30: Se pelo ID passado não existir um usuário no banco, deve retornar uma exceção com o erro "User not exists";
+  - TODO 31: Deve retornar TRUE se o usuário for atualizado corretamente;   
+*/
+}
+
+module.exports = { updateUser };
