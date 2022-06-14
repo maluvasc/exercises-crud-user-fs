adc9232e6bb38baeedf0cc38cc62c8ed0eaf33cd
Felipe Rosas <contato@eufelipe.com>
2022-06-14 12:31:12 -0300

    chore: add find users by email service

---
+++ src/services/users/find-user-by-email.service.js
@@ -0,0 +1,8 @@
+async function findUserByEmail(email) {
+  /*
+  - TODO 23: Deve retornar FALSE se o usuário com o email passado nao existir;
+  - TODO 24: Deve retornar um usuário válido se ele existir no banco;
+*/
+}
+
+module.exports = { findUserByEmail };
