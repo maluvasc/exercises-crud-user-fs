c704981012b2f7cdaadbedae4af4aad837e70537
Felipe Rosas <contato@eufelipe.com>
2022-06-14 12:30:09 -0300

    chore: add delete user service

---
+++ src/services/users/delete-user.service.js
@@ -0,0 +1,10 @@
+async function deleteUser(id) {
+  /*
+
+  - TODO 20: Deve retornar uma exceção de erro "User ID is required" se o ID não for passado;
+  - TODO 21: Se pelo ID passado não existir um usuário no banco, deve retornar uma exceção com o erro "User not exists";
+  - TODO 22: Com o sucesso na remoção, deve retornar TRUE
+*/
+}
+
+module.exports = { deleteUser };
