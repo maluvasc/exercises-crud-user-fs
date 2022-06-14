077a716e00d60eca92aac400edd668f1f74ac01f
Felipe Rosas <contato@eufelipe.com>
2022-06-14 00:22:08 -0300

    chore: add user repository

---
+++ src/repositories/user-repository.js
@@ -0,0 +1,14 @@
+async function loadUsersRepository() {}
+
+async function createUserRepository(user) {}
+
+async function updateUserRepository(id, data) {}
+
+async function deleteUserRepository(id) {}
+
+module.exports = {
+  loadUsersRepository,
+  createUserRepository,
+  deleteUserRepository,
+  updateUserRepository,
+};
